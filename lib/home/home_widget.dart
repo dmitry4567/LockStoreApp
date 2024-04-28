7fad00e31f845ae26fae4ef8ef8d90a0d7a51dfa
dmitry4567 <dimon2003pp@gmail.com>
2024-04-28 15:43:45 +0300

    make pageview on mobile

--- lib/home/home_widget.dart
+++ lib/home/home_widget.dart
@@ -4,6 +4,7 @@ import 'package:LockStore/flutter_flow/flutter_flow_widgets.dart';
 import 'package:LockStore/home/model.dart';
 import 'package:LockStore/layout/adaptive.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
 import '../../flutter_flow/custom_functions.dart' as functions;
 
 class HomeWidget extends StatefulWidget {
@@ -14,6 +15,9 @@ class HomeWidget extends StatefulWidget {
 }
 
 class _HomeWidgetState extends State<HomeWidget> {
+  PageController controller = PageController(initialPage: 0);
+  int _activePage = 0;
+
   Future<dynamic> getData() async {
     final response =
         await GetAllProject.call(token: FFAppState().userAuthToken);
@@ -158,110 +162,124 @@ class _HomeWidgetState extends State<HomeWidget> {
                 ],
               ),
             )
-          : Container(
-              padding:
-                  EdgeInsets.only(top: MediaQuery.of(context).padding.top + 1),
+          : SingleChildScrollView(
               child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Divider(
-                    height: 0,
-                    indent: 0,
-                    thickness: 1,
-                    color: Colors.grey.shade300,
-                  ),
-                  Center(
-                    child: Image.asset(
-                      "assets/images/lock1.jpg",
-                      width: 500,
-                      fit: BoxFit.cover,
+                mainAxisSize: MainAxisSize.min,
+                children: <Widget>[
+                  SizedBox(
+                    height: MediaQuery.of(context).size.height * 0.686,
+                    child: PageView(
+                      controller: controller,
+                      onPageChanged: (int page) {
+                        _activePage = page;
+                      },
+                      children: const [
+                        PromoWidget(),
+                        PromoWidget(),
+                        PromoWidget(),
+                      ],
                     ),
                   ),
-                  const SizedBox(
-                    height: 20,
-                  ),
                   Padding(
-                    padding: const EdgeInsets.symmetric(horizontal: 16),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
+                    padding: EdgeInsets.symmetric(vertical: 24, horizontal: 8),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
                       children: [
-                        Text(
-                          "Golden Soft\nGS-200Z-5 для офиса",
-                          style: TextStyle(
-                            color: Color(0xff161C24),
-                            fontSize: 20,
-                            fontFamily: "SF",
-                            fontWeight: FontWeight.w600,
+                        InkWell(
+                          onTap: () {
+                            setState(() {
+                              _activePage -= 1;
+                              controller.previousPage(
+                                duration: Durations.long1,
+                                curve: Curves.ease,
+                              );
+                            });
+                          },
+                          customBorder: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(100),
+                          ),
+                          child: Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child:
+                                SvgPicture.asset("assets/icons/arrow_left.svg"),
                           ),
                         ),
-                        const SizedBox(
-                          height: 24,
-                        ),
-                        Text(
-                          "Цена",
-                          style: TextStyle(
-                            color: Color(0xff161C24),
-                            fontSize: 10,
-                            fontFamily: "SF",
-                            fontWeight: FontWeight.w400,
-                          ),
-                        ),
-                        const SizedBox(
-                          height: 4,
-                        ),
-                        Row(
-                          children: [
-                            Text(
-                              "33 000₽",
-                              style: TextStyle(
-                                color: Color(0xff161C24),
-                                fontSize: 22,
-                                fontFamily: "SF",
-                                fontWeight: FontWeight.w600,
-                              ),
-                            ),
-                            const SizedBox(
-                              width: 12,
-                            ),
-                            Text(
-                              "37 000₽",
-                              style: TextStyle(
-                                color: Color(0xffA4A4A4),
-                                fontSize: 20,
-                                fontFamily: "SF",
-                                fontWeight: FontWeight.w300,
-                                decoration: TextDecoration.lineThrough,
-                                decorationColor: Color(0xffA4A4A4),
-                              ),
-                            ),
-                          ],
-                        ),
+                        const Spacer(),
                         SizedBox(
-                          height: 16,
+                          height: 24,
+                          child: ListView.builder(
+                            itemCount: 3,
+                            shrinkWrap: true,
+                            scrollDirection: Axis.horizontal,
+                            itemBuilder: (context, index) {
+                              return Container(
+                                margin:
+                                    const EdgeInsets.symmetric(horizontal: 8),
+                                decoration: BoxDecoration(
+                                  shape: BoxShape.circle,
+                                  color: _activePage == index
+                                      ? const Color(0xff4295E4)
+                                      : const Color(0xffADD7FF),
+                                ),
+                                width: 12,
+                                height: 12,
+                              );
+                            },
+                          ),
                         ),
-                        Container(
-                          width: double.infinity,
-                          height: 50,
-                          child: FFButtonWidget(
-                            onPressed: () async {},
-                            text: 'Добавить в корзину',
-                            options: FFButtonOptions(
-                              width: double.infinity,
-                              height: double.infinity,
-                              color: Color(0xff4295E4),
-                              elevation: 0,
-                              textStyle: const TextStyle(
-                                fontFamily: 'SF',
-                                color: Colors.white,
-                                fontSize: 14,
-                                fontWeight: FontWeight.w500,
-                              ),
-                            ),
+                        const Spacer(),
+                        InkWell(
+                          onTap: () {
+                            setState(() {
+                              _activePage += 1;
+                              controller.nextPage(
+                                duration: Durations.long1,
+                                curve: Curves.ease,
+                              );
+                            });
+                          },
+                          customBorder: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(100),
+                          ),
+                          child: Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: SvgPicture.asset(
+                                "assets/icons/arrow_right.svg"),
                           ),
                         ),
                       ],
                     ),
-                  )
+                  ),
+                  Card(
+                    child: ListTile(
+                        title: Text('Motivation $int'),
+                        subtitle:
+                            Text('this is a description of the motivation')),
+                  ),
+                  Card(
+                    child: ListTile(
+                        title: Text('Motivation $int'),
+                        subtitle:
+                            Text('this is a description of the motivation')),
+                  ),
+                  Card(
+                    child: ListTile(
+                        title: Text('Motivation $int'),
+                        subtitle:
+                            Text('this is a description of the motivation')),
+                  ),
+                  Card(
+                    child: ListTile(
+                        title: Text('Motivation $int'),
+                        subtitle:
+                            Text('this is a description of the motivation')),
+                  ),
+                  Card(
+                    child: ListTile(
+                        title: Text('Motivation $int'),
+                        subtitle:
+                            Text('this is a description of the motivation')),
+                  ),
                 ],
               ),
             ),
@@ -269,6 +287,122 @@ class _HomeWidgetState extends State<HomeWidget> {
   }
 }
 
-    // final isDesktop = isDisplayDesktop(context);
-    //     if (isDesktop) {}
-    //     else {}
+class PromoWidget extends StatelessWidget {
+  const PromoWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.black.withOpacity(0.02),
+      padding: EdgeInsets.only(top: MediaQuery.of(context).padding.top + 1),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Divider(
+            height: 0,
+            indent: 0,
+            thickness: 1,
+            color: Colors.grey.shade300,
+          ),
+          Center(
+            child: Image.asset(
+              "assets/images/lock1.jpg",
+              width: 320,
+              fit: BoxFit.fill,
+            ),
+          ),
+          const SizedBox(
+            height: 20,
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 16),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  "Golden Soft\nGS-200Z-5 для офиса",
+                  style: TextStyle(
+                    color: Color(0xff161C24),
+                    fontSize: 20,
+                    fontFamily: "SF",
+                    fontWeight: FontWeight.w600,
+                  ),
+                ),
+                const SizedBox(
+                  height: 24,
+                ),
+                Text(
+                  "Цена",
+                  style: TextStyle(
+                    color: Color(0xff161C24),
+                    fontSize: 10,
+                    fontFamily: "SF",
+                    fontWeight: FontWeight.w400,
+                  ),
+                ),
+                const SizedBox(
+                  height: 4,
+                ),
+                Row(
+                  children: [
+                    Text(
+                      "33 000₽",
+                      style: TextStyle(
+                        color: Color(0xff161C24),
+                        fontSize: 22,
+                        fontFamily: "SF",
+                        fontWeight: FontWeight.w600,
+                      ),
+                    ),
+                    const SizedBox(
+                      width: 12,
+                    ),
+                    Text(
+                      "37 000₽",
+                      style: TextStyle(
+                        color: Color(0xffA4A4A4),
+                        fontSize: 20,
+                        fontFamily: "SF",
+                        fontWeight: FontWeight.w300,
+                        decoration: TextDecoration.lineThrough,
+                        decorationColor: Color(0xffA4A4A4),
+                      ),
+                    ),
+                  ],
+                ),
+                SizedBox(
+                  height: 16,
+                ),
+                Container(
+                  width: double.infinity,
+                  height: 50,
+                  child: FFButtonWidget(
+                    onPressed: () async {},
+                    text: 'Добавить в корзину',
+                    options: FFButtonOptions(
+                      width: double.infinity,
+                      height: double.infinity,
+                      color: Color(0xff4295E4),
+                      elevation: 0,
+                      textStyle: const TextStyle(
+                        fontFamily: 'SF',
+                        color: Colors.white,
+                        fontSize: 14,
+                        fontWeight: FontWeight.w500,
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
+
+// final isDesktop = isDisplayDesktop(context);
+//     if (isDesktop) {}
+//     else {}
